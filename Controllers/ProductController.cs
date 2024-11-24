12b3e140de1ad0e1964cdaf410f2998fadf2b26f
Mina <Mina@MINA>
2024-11-24 05:46:56 +0200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Data.Entity;
+using Store.Repository.InterFace;
+using Store.Service.Services.product;
+using Store.Service.Services.product.Dtos;
+
+namespace Store.API.Controllers
+{
+
+    public class ProductController : BassApiController
+    {
+        private readonly IGenaricRepo<Product> _genaricRepo;
+
+        public ProductController(IGenaricRepo<Product> productRepo)
+        {
+            _genaricRepo = productRepo;
+        }
+
+        // /api/Product
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+        {
+            var product = await _genaricRepo.GetAllAsync();
+            return Ok(product);
+        }
+        // /api/Product/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            var productById = await _genaricRepo.GetAsync(id);
+            if (productById == null)
+                return NotFound();  
+
+            return Ok(productById);
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Data.Context;
+using Store.Repository.InterFace;
+using Store.Repository.Repository;
+using Store.Repository.SeedData;
+
+namespace Store.API
+{
+    public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+
+			#region configuration services
+			builder.Services.AddControllers();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			#endregion
+
+
+			#region Connecting to DataBase
+			builder.Services.AddDbContext<StoreDbContext>(Options =>
+				{
+					Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+				});
+			#endregion
+
+			#region Allow Dependancy injection
+			builder.Services.AddScoped(typeof(IGenaricRepo<>), typeof(GenaricRepo<>));
+			#endregion
+
+			var app = builder.Build();
+			//to update database by defult
+		    var scope = app.Services.CreateScope();
+			var Services = scope.ServiceProvider;
+			var _dbContext = Services. GetRequiredService<StoreDbContext>();
+			// exption looger 
+			var loggerFactory = Services.GetRequiredService<ILoggerFactory>();
+
+
+			try
+			{
+			    await _dbContext.Database.MigrateAsync(); //apply migration
+				await StoreContextSeed.SeedAsync(_dbContext);//apply Seeding
+            }
+			catch (Exception Ex)
+			{
+
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(Ex, "Error in Migration");
+			}
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
