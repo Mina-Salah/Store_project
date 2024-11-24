8fd46768cbefee1e0ad9c9240463f83092eea1ef
Mina <Mina@MINA>
2024-11-24 12:33:45 +0200

    Refactor ProductController to use Specification Pattern

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Store.Data.Entity;
 using Store.Repository.InterFace;
+using Store.Repository.Repository;
+using Store.Repository.Specification;
 using Store.Service.Services.product;
 using Store.Service.Services.product.Dtos;
 
@@ -11,7 +13,9 @@ namespace Store.API.Controllers
     public class ProductController : BassApiController
     {
         private readonly IGenaricRepo<Product> _genaricRepo;
-
+        
+        
+        // Constructor: Inject the generic repository for Product
         public ProductController(IGenaricRepo<Product> productRepo)
         {
             _genaricRepo = productRepo;
@@ -21,18 +25,23 @@ namespace Store.API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
         {
-            var product = await _genaricRepo.GetAllAsync();
+            // Apply specification with filters for brand and category
+            var spec = new ProductSpecification();
+            var product = await _genaricRepo.GetAllWithSpecAsync(spec);
             return Ok(product);
         }
         // /api/Product/id
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetById(int id)
         {
-            var productById = await _genaricRepo.GetAsync(id);
+            // Apply specification to fetch the product by its ID
+            var spec = new ProductSpecification();
+            var productById = await _genaricRepo.GetWithSpecAsync(spec);
+            
             if (productById == null)
-                return NotFound();  
+                return NotFound();// Return 404 if the product doesn't exist  
 
-            return Ok(productById);
+            return Ok(productById);// Return the product if found
         }
 
 
