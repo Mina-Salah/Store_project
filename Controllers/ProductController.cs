0b4884f08b29e734914c1fd0dbe671cde4f3707d
Mina <Mina@MINA>
2024-12-25 20:18:45 +0200

    sorting ,filter,pagination

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -6,21 +6,17 @@ using Store.API.Error;
 using Store.Data.Entity;
 using Store.Repository.InterFace;
 using Store.Repository.Repository;
-using Store.Repository.Specification;
-using Store.Service.Services.product;
-using Store.Service.Services.product.Dtos;
+using Store.Repository.Specification.product;
 
 namespace Store.API.Controllers
 {
-
     public class ProductController : BassApiController
     {
         private readonly IGenaricRepo<Product> _productRepo;
         private readonly IMapper _mapper;
 
-
         // Constructor: Inject the generic repository for Product
-        public ProductController(IGenaricRepo<Product> productRepo , IMapper mapper)
+        public ProductController(IGenaricRepo<Product> productRepo, IMapper mapper)
         {
             _productRepo = productRepo;
             _mapper = mapper;
@@ -30,9 +26,9 @@ namespace Store.API.Controllers
         [ProducesResponseType(typeof(IEnumerable<ProductToReturnDto>), 200)]
         [ProducesResponseType(typeof(ApiResponse), 404)]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductToReturnDto>>> GetProduct(string sort)
+        public async Task<ActionResult<IEnumerable<ProductToReturnDto>>> GetProducts([FromQuery] ProductQueryParameters parameters)
         {
-            var spec = new ProductSpecification(sort);
+            var spec = new ProductSpecification(parameters);
             var products = await _productRepo.GetAllWithSpecAsync(spec);
 
             if (!products.Any())
@@ -40,10 +36,13 @@ namespace Store.API.Controllers
                 return NotFound(new ApiResponse(404));
             }
 
-            return Ok(_mapper.Map<IEnumerable<Product>, IEnumerable<ProductToReturnDto>>(products));
+            var mappedProducts = _mapper.Map<IEnumerable<Product>, IEnumerable<ProductToReturnDto>>(products);
+
+            return Ok(mappedProducts);
         }
+
         // /api/Product/id
-        [ProducesResponseType(typeof(IEnumerable<ProductToReturnDto>), 200)]
+        [ProducesResponseType(typeof(ProductToReturnDto), 200)]
         [ProducesResponseType(typeof(ApiResponse), 404)]
         [HttpGet("{id}")]
         public async Task<ActionResult<ProductToReturnDto>> GetById(int id)
@@ -51,15 +50,11 @@ namespace Store.API.Controllers
             // Apply specification to fetch the product by its ID
             var spec = new ProductSpecification(id);
             var productById = await _productRepo.GetWithSpecAsync(spec);
-            
+
             if (productById == null)
-                return NotFound(new ApiResponse(404));// Return 404 if the product doesn't exist  
+                return NotFound(new ApiResponse(404)); // Return 404 if the product doesn't exist  
 
-            return Ok(_mapper.Map<Product,ProductToReturnDto>(productById));// Return the product if found
+            return Ok(_mapper.Map<Product, ProductToReturnDto>(productById)); // Return the product if found
         }
-
-
-
-
     }
 }
